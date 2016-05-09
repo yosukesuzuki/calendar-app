d2e5677835df051b46fd21a0f5048d2935142df0
SatowTakeshi <tsurumurasaki@outlook.com>
2016-05-10 00:05:43 +0900

    finish red

--- project/main/tests/test_views.py
+++ project/main/tests/test_views.py
@@ -16,20 +16,34 @@ class DownloadicsTest(GAETestBase):
         app = get_application()
         self.client = Client(app, BaseResponse)
 
-        self.test_values = {
+        self.test_values_0 = {
             'date': datetime.datetime(2016, 5, 20, 15, 0),
             'title': 'THIS IS TITLE',
-            'description': 'THIS IS TITLE',
+            'description': 'THIS IS SUMMARY',
         }
 
-        eve = Event(
-            event_date=self.test_values['date'],
-            title=self.test_values['title'],
-            description=self.test_values['description'],
+        self.test_values_1 = {
+            'date': datetime.datetime(2016, 6, 20, 15, 0),
+            'title': 'THIS IS ALSO TITLE',
+            'description': 'THIS IS ALSO SUMMARY',
+        }
+
+        eve_0 = Event(
+            event_date=self.test_values_0['date'],
+            title=self.test_values_0['title'],
+            description=self.test_values_0['description'],
         )
-        eve.put()
+
+        eve_1 = Event(
+            event_date=self.test_values_1['date'],
+            title=self.test_values_1['title'],
+            description=self.test_values_1['description'],
+        )
+
+        eve_0.put()
+        eve_1.put()
         events = Event.all().fetch(100)
-        self.assertEquals(len(events), 1)
+        self.assertEquals(len(events), 2)
         self.assertEquals(events[0].title, 'THIS IS TITLE')
         self.event_key = str(events[0].key())
 
@@ -41,7 +55,20 @@ class DownloadicsTest(GAETestBase):
         downloaded = res.get_data()
         reparsed = icalendar.Calendar.from_ical(downloaded)
         reparsed_eve = reparsed.walk('VEVENT')[0]
-        stringfied = self.test_values['date'].strftime('%Y%m%dT%H%M%S')
-        self.assertEquals(reparsed_eve['summary'].to_ical(), self.test_values['title'])
+        stringfied = self.test_values_0['date'].strftime('%Y%m%dT%H%M%S')
+        self.assertEquals(reparsed_eve['summary'].to_ical(), self.test_values_0['title'])
         self.assertEquals(reparsed_eve['dtstart'].to_ical(), stringfied)
-        self.assertEquals(reparsed_eve['description'].to_ical(), self.test_values['description'])
+        self.assertEquals(reparsed_eve['description'].to_ical(), self.test_values_0['description'])
+
+    def test_download_all_events_icsfile(self):
+        target_url = urlparse.urljoin('/ical/', 'all')
+        res = self.client.get(target_url)
+        self.assertEquals(res.status_code, 200)
+
+        downloaded = res.get_data()
+        reparsed = icalendar.Calendar.from_ical(downloaded)
+        reparsed_eve = reparsed.walk('VEVENT')[1]
+        stringfied = self.test_values_1['date'].strftime('%Y%m%dT%H%M%S')
+        self.assertEquals(reparsed_eve['summary'].to_ical(), self.test_values_1['title'])
+        self.assertEquals(reparsed_eve['dtstart'].to_ical(), stringfied)
+        self.assertEquals(reparsed_eve['description'].to_ical(), self.test_values_1['description'])
